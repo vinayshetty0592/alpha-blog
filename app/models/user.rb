60427ac515267a3512697160b1626e5d98f3dafc
vinayshetty0592 <vinay.shetty0592@gmail.com>
2017-03-18 11:36:50 +0530

    Rearrange lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base 
 	validates :username, presence: true, 
-						length: {minimum: 3, maximum: 25},
 						uniqueness: { case_sensitive: true }
+						length: {minimum: 3, maximum: 25},
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true,
-						length: {maximum: 105},
 						uniqueness: { case_sensitive: true },
+						length: {maximum: 105},
 						format: { with: VALID_EMAIL_REGEX}
 end
