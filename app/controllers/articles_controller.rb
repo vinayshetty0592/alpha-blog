6bf58b8a074859d32f43c0d710eae02108f4c1f8
vinayshetty0592 <vinay.shetty0592@gmail.com>
2017-03-22 22:32:02 +0530

    Create articles for logged in user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:success] = "Article saved successfully."
 			redirect_to article_path(@article)
